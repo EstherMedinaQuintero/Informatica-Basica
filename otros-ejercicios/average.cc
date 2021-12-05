1e3146cb8927c7fda696885d89cb63a7127a6f3b
Esther M. Quintero <alu0101434780@ull.edu.es>
2021-12-05 16:58:36 +0000

    Actualizando.

--- otros-ejercicios/average.cc
+++ otros-ejercicios/average.cc
@@ -10,8 +10,6 @@
  */
 
 #include <iostream>
-#include <cstring>
-#include <vector>
 #include <iomanip>
 
 // @brief Esta función escribe el mensaje inicial con la explicación del programa
@@ -20,36 +18,14 @@ int MensajeInicial() {
   return 0;
 }
 
-// @brief Esta función separa un string por los espacios
-// @param frase Es el string que queremos separar
-std::vector<std::string> SeparaPorEstacios(std::string& frase) {
-  std::string separador{" "};
-  std::vector<std::string> separado{};
-  size_t pos = 0; // Almacena la posición del espacio que encontrará
-  while ((pos = frase.find(separador)) != std::string::npos) {
-    separado.push_back(frase.substr(0, pos));
-    frase.erase(0, pos + separador.length());
-  }
-  separado.push_back(frase); // Añade el último elemento
-  // for (const auto &str : separado) {
-  //   std::cout << "Elemento:" << str << std::endl;
-  // }
-  return separado;
-}
-
 int main() {
   // MensajeInicial()
-  std::string numeros_introducidos;
-  std::getline(std::cin, numeros_introducidos);
-  std::vector<std::string> numeros_separados{SeparaPorEstacios(numeros_introducidos)};
+  int numero_introducido;
   double media{0};
   double suma{0};
-  // std::cout << "Tamaño vector: " << numeros_separados.size() << std::endl;
-  for (int contador{0}; contador < numeros_separados.size(); ++contador) {
-    suma += std::stod(numeros_separados[contador]);
-    // std::cout << "Elemento vector: " << numeros_separados[contador] << std::endl;
-  }
-  media = suma / double(numeros_separados.size());
+  int cantidad_numeros{0};
+  for (int contador )
+  media = suma / double(cantidad_numeros);
   std::cout << std::fixed << std::setprecision(2) << media << std::endl;
   return 0;
 }
